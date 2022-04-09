9bcabe02272e4d405087b61117ed497661775631
AntonKasatkin26 <102464311+AntonKasatkin26@users.noreply.github.com>
2022-04-09 19:28:38 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,20 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+import random
+list_elements = []
+counter = 0
+amount_elements = int(input("введите количество элементов списка: "))
+while counter < amount_elements:
+    list_elements.append(random.randint(-100, 100))
+    counter += 1
+print("Сгенерированные элементы списка: ", list_elements)
+
+summa_elements = 0
+for element in list_elements:
+    if element > 0 and element % 2 == 0:
+        summa_elements += element
+    else:
+        summa_elements = 0
+
+print("Cумма элементов списка кратных двум = ", summa_elements)
