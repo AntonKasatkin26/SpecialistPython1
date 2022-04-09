d7840e11086d32b30128b0b1ff7f62396a1424f9
AntonKasatkin26 <102464311+AntonKasatkin26@users.noreply.github.com>
2022-04-09 18:36:54 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -4,6 +4,15 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+new_names = ''
+i=1
+for name in names:
+    if i != len(names):
+        new_names = new_names + name + ", "
+    else:
+        new_names = new_names + name
+    i+=1
+print(new_names)
 
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
