187518103a0f110b0fc72abc5e7ff7e538e7bc78
AntonKasatkin26 <102464311+AntonKasatkin26@users.noreply.github.com>
2022-04-10 15:59:56 +0300

    Update 04_task_func.py

--- Module4/practice/funcs_part1/04_task_func.py
+++ Module4/practice/funcs_part1/04_task_func.py
@@ -10,3 +10,9 @@ def can_triangle(p1, p2, p3):
 can_triangle((10, 12), (14, 18), (12, 12))
 
 # Не забудьте протестировать вашу функцию
+
+
+def can_triangle(p1, p2, p3):
+    # TODO: your code here
+    return (p1 + p2) > p3 or (p1 + p3) > p2
+print(can_triangle((4, 4), (4, 4), (13, 13)))
